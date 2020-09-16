a63ca4284cea01c6518e0b40d57ea14e877ae67a
hà phạm <phamhabnkx@gmail.com>
2020-09-16 14:59:29 +0700

    Updates

---
+++ precache-manifest.8d348bbc867e43ff7ee28917aeba81af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15298c69bac8cb08a56f26de05d625b",
+    "url": "/ha-ptt0601.github.io/oe-test/index.html"
+  },
+  {
+    "revision": "59d0ca3c6cd7fdafe217",
+    "url": "/ha-ptt0601.github.io/oe-test/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "78882f6e81db087f8090",
+    "url": "/ha-ptt0601.github.io/oe-test/static/css/main.f1feeacd.chunk.css"
+  },
+  {
+    "revision": "59d0ca3c6cd7fdafe217",
+    "url": "/ha-ptt0601.github.io/oe-test/static/js/2.c02d9ab1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/ha-ptt0601.github.io/oe-test/static/js/2.c02d9ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78882f6e81db087f8090",
+    "url": "/ha-ptt0601.github.io/oe-test/static/js/main.b8cc5023.chunk.js"
+  },
+  {
+    "revision": "4277febfe83ee8f9ba1e",
+    "url": "/ha-ptt0601.github.io/oe-test/static/js/runtime-main.858e2587.js"
+  }
+]);
